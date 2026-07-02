8731ee7807e4d787b32919298b59470f0fcf987e
skyshanhu <160482842+skyshanhu@users.noreply.github.com>
2026-07-02 22:01:03 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main style={{
+      padding:"80px",
+      textAlign:"center"
+    }}>
+      <h1>Nelorix Machinery</h1>
+
+      <p>
+        Industrial Machinery Solutions for Global Markets
+      </p >
+
+      <div style={{marginTop:"30px"}}>
+        <a href=" ">
+          Explore Products
+        </a >
+      </div>
+    </main>
+  )
+}
